5e707d5bfb5ba648d666037c15ae500696d984f3
hallgato <hallgato@LABOR215-15>
2022-03-22 16:55:50 +0100

    init server

---
+++ server/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public string Sender { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+    }
+}
