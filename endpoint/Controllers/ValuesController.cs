e5967c378ad6fde48ad221f80e4675923ec5a370
hallgato <hallgato@LABOR215-15>
2022-03-22 17:30:40 +0100

    apis init

---
+++ endpoint/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Logic;
+using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        IChatLogic logic;
+
+        public ChatController(IChatLogic l)
+        {
+            this.logic = l;
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> ReadAll()
+        {
+            return logic.GetAllMessages();
+        }
+
+        [HttpPost]
+
+        public void SendAMessage(Message m)
+        {
+            logic.SendMessage(m);
+        }
+    }
+}
---
+++ server/Logic/IChatLogic.cs
@@ -0,0 +1,11 @@
+﻿using server.Models;
+using System.Collections.Generic;
+
+namespace server.Logic
+{
+    public interface IChatLogic
+    {
+        IList<Message> GetAllMessages();
+        void SendMessage(Message m);
+    }
+}
