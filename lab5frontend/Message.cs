bfa91b7eb0c54a3c5dc7c0d734c972927802e115
hallgato2 <hallgato@LABOR215-16>
2022-03-22 17:44:40 +0100

    Commit

---
+++ lab5frontend/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5frontend
+{
+     public class Message
+    {
+        public string Text { get; set; }
+        public string Sender { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
